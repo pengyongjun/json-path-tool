ac25997e870dbb87410cb79ae6632ce442de0978
pengyongjun <pengyongjun3@163.com>
2022-12-18 18:02:30 +0800

    init project

---
+++ src/main/java/com/coolqa/jsonpathtool/JsonPathToolApplication.java
@@ -0,0 +1,13 @@
+package com.coolqa.jsonpathtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonPathToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonPathToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coolqa/jsonpathtool/JsonPathToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolqa.jsonpathtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonPathToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
